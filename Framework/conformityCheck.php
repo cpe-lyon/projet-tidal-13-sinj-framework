684e30e42a00b2434b3b87c1cb0b48928fd8a225
Julien Cpsn <julien.caposiena@etu.univ-lyon1.fr>
2022-09-16 01:20:14 +0200

    Updated conformityCheck.php

--- Framework/conformityCheck.php
+++ Framework/conformityCheck.php
@@ -1,10 +1,11 @@
 <?php
 
-// Autoloader
-require(__DIR__ . '/../autoloader.php');
+echo "Checking app conformity...\n";
 
-// Get every user defined route
-$routes = require("Config/routes.php");
+// Autoloader
+use Framework\Route;
+
+require(__DIR__ . '/../autoloader.php');
 
 // Import every user controller
 foreach (glob("Controllers/*.php") as $filename)
@@ -12,8 +13,51 @@ foreach (glob("Controllers/*.php") as $filename)
     include_once($filename);
 }
 
+/* === CONFIG === */
+echo " ·Config\n";
+
+require("Config/config.php");
+
+if (APP_NAME === 'APP_NAME') {
+    exit();
+}
+if (!is_string(APP_NAME)) {
+    echo "[Error] APP_NAME must be a string\n";
+    exit();
+}
+if (APP_NAME === 'APP_URL') {
+    exit();
+}
+if (!is_string(APP_URL)) {
+    echo "[Error] APP_URL must be a string\n";
+    exit();
+}
+if (APP_NAME === 'APP_PORT') {
+    exit();
+}
+if (!is_integer(APP_PORT)) {
+    echo "[Error] APP_PORT must be a integer\n";
+    exit();
+}
+echo "    OK\n";
+
+/* === ROUTES === */
+echo " ·Routes\n";
+
+// Get every user defined route
+$routes = require("Config/routes.php");
+
+if (!is_array($routes)) {
+    echo "[Error] routes.php must return an array\n";
+    exit();
+}
+
 // Checking routes conformity
 foreach ($routes as $index => $route) {
+    if (!$route instanceof Route){
+        echo "[Error] Given data is not an instance of Route for entry N°" . ($index + 1) . " in routes.php\n";
+        exit();
+    }
     if (empty($route->getMethod())) {
         echo "[Error] Route method can't be empty for route N°" . ($index + 1) . " in routes.php\n";
         exit();
@@ -39,3 +83,50 @@ foreach ($routes as $index => $route) {
         exit();
     }
 }
+echo "    OK\n";
+
+/* === VIEWS === */
+
+/* === DATABASE === */
+echo " ·Database\n";
+require("Config/database.php");
+
+if (DB_HOST === 'DB_HOST') {
+    exit();
+}
+if (!is_string(DB_HOST)) {
+    echo "[Error] DB_HOST must be a string\n";
+    exit();
+}
+if (DB_PORT === 'DB_PORT') {
+    exit();
+}
+if (!is_integer(DB_PORT)) {
+    echo "[Error] DB_PORT must be a integer\n";
+    exit();
+}
+if (DB_NAME === 'DB_NAME') {
+    exit();
+}
+if (!is_string(DB_NAME)) {
+    echo "[Error] DB_NAME must be a string\n";
+    exit();
+}
+if (DB_USER === 'DB_USER') {
+    exit();
+}
+if (!is_string(DB_USER)) {
+    echo "[Error] DB_USER must be a string\n";
+    exit();
+}
+if (DB_PASSWORD === 'DB_PASSWORD') {
+    exit();
+}
+if (!is_string(DB_PASSWORD)) {
+    echo "[Error] DB_PASSWORD must be a string\n";
+    exit();
+}
+echo "    OK\n";
+
+
+echo "The app is conform\n";
