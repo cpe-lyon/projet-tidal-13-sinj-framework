5242570cc2b3b7c3816f954d1b6377b84e90e187
Julien Cpsn <julien.caposiena@etu.univ-lyon1.fr>
2022-09-16 01:21:13 +0200

    Changed DB_PORT to integer

--- Config/database.php
+++ Config/database.php
@@ -1,7 +1,7 @@
 <?php
 
 define('DB_HOST', 'localhost');
-define('DB_PORT', '5432');
+define('DB_PORT', 5432);
 define('DB_NAME', 'sinj');
 define('DB_USER', 'sinj');
 define('DB_PASSWORD', 'bonobo');
