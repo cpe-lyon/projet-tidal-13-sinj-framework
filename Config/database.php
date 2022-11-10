cf109a8dbb690d658671f8eeab24638049cfb732
FranCHEVAL <francois.cheval@cpe.fr>
2022-11-10 14:10:34 +0100

    Co-authored-by: Tanguy Frouin <SlendyBoy@users.noreply.github.com>

--- Config/database.php
+++ Config/database.php
@@ -1,7 +1,7 @@
 <?php
 
 define('DB_DRIVER', 'pgsql');
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'winhost');
 define('DB_PORT', 5432);
 define('DB_NAME', 'sinj');
 define('DB_USER', 'sinj');
